41bc14c56437a620fe0b5e06f3541a0b4a873a8f
Marcos Ruiz <marcosruizgarcia@gmail.com>
2021-03-15 17:59:44 +0100

    Añado ejemplos de eventos.

--- src/main/java/propiedadesRestringidas/BeanReceptor.java
+++ src/main/java/propiedadesRestringidas/BeanReceptor.java
@@ -7,7 +7,7 @@ public class BeanReceptor implements VetoableChangeListener {
   public void vetoableChange(PropertyChangeEvent event) throws PropertyVetoException {
     // comprobación de las condiciones
     // TODO
-    // se lanza exceptción si no se aprueba el cambio
+    // se lanza excepción si no se aprueba el cambio
     throw new PropertyVetoException("mensaje", event);
   }
 }
